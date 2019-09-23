2c5a8f19fec066a3c4e9680d99ea13619d5bda86
Denis Melnikov <066den@gmail.com>
2019-09-23 16:22:56 +0300

    upgrade

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	$('.phone').on('click', function(){
-		$(this).toggleClass('active');
-	})
+	$('a[href="#"]').on('click', function(e) { e.preventDefault(); })
 
 	$('.main-slider .owl-carousel').owlCarousel({
 		items: 1
@@ -44,9 +42,23 @@ document.addEventListener("DOMContentLoaded", function() {
 		zoom: {
 			enabled: true,
 			duration: 300, // don't foget to change the duration also in CSS
-			
 		}
-		
+	});
+
+	$('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		mainClass: 'mfp-zoom-in',
+		focus: '#name',
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
 	});
 
 	$(window).scroll(function(){
@@ -56,6 +68,40 @@ document.addEventListener("DOMContentLoaded", function() {
 		} else {
 			$('.navbar').removeClass('stuck');
 		}
+	})
+
+	var inputs = Array.prototype.slice.call(document.querySelectorAll('.phone-mask'));
+	inputs.forEach( function(input) {
+		var keyCode;
+		function mask(event) {
+			event.keyCode && (keyCode = event.keyCode);
+			var pos = this.selectionStart;
+			if (pos < 4) event.preventDefault();
+			var matrix = "+7 (___) ___-____",
+					i = 0,
+					def = matrix.replace(/\D/g, ""),
+					val = this.value.replace(/\D/g, ""),
+					new_value = matrix.replace(/[_\d]/g, function(a) {
+						return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+												});
+					i = new_value.indexOf("_");
+			if (i != -1) {
+				i < 5 && (i = 4);
+				new_value = new_value.slice(0, i)
+			}
+			var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+				function(a) {
+					return "\\d{1," + a.length + "}"
+				}).replace(/[+()]/g, "\\$&");
+			reg = new RegExp("^" + reg + "$");
+			if (!reg.test(this.value) || this.value.length < 6 || keyCode > 47 && keyCode < 58) this.value = new_value;
+			if (event.type == "blur" && this.value.length < 6)  this.value = "";
+		}
+
+		input.addEventListener("input", mask, false);
+		input.addEventListener("focus", mask, false);
+		input.addEventListener("blur", mask, false);
+		input.addEventListener("keydown", mask, false)
 	})
 
 });
