0a7c7f280ea6805b08f776e34c4ee490711e46c7
Denis <066den@gmail.com>
2019-11-15 06:37:43 +0200

    15.11

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -3,7 +3,18 @@ document.addEventListener("DOMContentLoaded", function() {
 	$('a[href="#"]').on('click', function(e) { e.preventDefault(); })
 
 	$('.main-slider .owl-carousel').owlCarousel({
-		items: 1
+		items: 1,
+		animateOut: 'fadeOut',
+		animateIn: 'fadeUp',
+		loop: true,
+		autoplay: true,
+		smartSpeed:450
+    
+	})
+
+	$('.gallery .owl-carousel').owlCarousel({
+		items: 1,
+		
 	})
 
 	$('.brand-list .owl-carousel').owlCarousel({
@@ -14,6 +25,10 @@ document.addEventListener("DOMContentLoaded", function() {
 		autoplayTimeout:1500,
 		responsiveClass:true,
 		responsive:{
+			0:{
+				items: 2,
+			},
+			576:{},
 			990:{
 				margin: 48,
 			},	
@@ -92,9 +107,13 @@ document.addEventListener("DOMContentLoaded", function() {
 			$(control_el).removeClass('disabled');
 		} else {
 			$(control_el).addClass('disabled');
-	}
+		}
+	});
 
-});
+	$('.hamburger').on('click', function(){
+		$(this).toggleClass('active');
+		$('#navbar .navbar-nav').slideToggle();
+	})
 
 	$(window).scroll(function(){
 		var topHeight = $('.header').height();
@@ -105,41 +124,98 @@ document.addEventListener("DOMContentLoaded", function() {
 		}
 	})
 
-	var inputs = Array.prototype.slice.call(document.querySelectorAll('.phone-mask'));
-	inputs.forEach( function(input) {
-		var keyCode;
-		function mask(event) {
-			event.keyCode && (keyCode = event.keyCode);
-			var pos = this.selectionStart;
-			if (pos < 4) event.preventDefault();
-			var matrix = "+7 (___) ___-____",
-					i = 0,
-					def = matrix.replace(/\D/g, ""),
-					val = this.value.replace(/\D/g, ""),
-					new_value = matrix.replace(/[_\d]/g, function(a) {
-						return i < val.length ? val.charAt(i++) || def.charAt(i) : a
-												});
-					i = new_value.indexOf("_");
-			if (i != -1) {
-				i < 5 && (i = 4);
-				new_value = new_value.slice(0, i)
-			}
-			var reg = matrix.substr(0, this.value.length).replace(/_+/g,
-				function(a) {
-					return "\\d{1," + a.length + "}"
-				}).replace(/[+()]/g, "\\$&");
-			reg = new RegExp("^" + reg + "$");
-			if (!reg.test(this.value) || this.value.length < 6 || keyCode > 47 && keyCode < 58) this.value = new_value;
-			if (event.type == "blur" && this.value.length < 6)  this.value = "";
-		}
-
-		input.addEventListener("input", mask, false);
-		input.addEventListener("focus", mask, false);
-		input.addEventListener("blur", mask, false);
-		input.addEventListener("keydown", mask, false)
-	})
-
 });
+
+let addPrice = (event) => {
+	if(event) {
+		let el = event.currentTarget;
+		if(el.getAttribute('type') == 'checkbox') {
+			let siblings = el.parentElement.parentElement.children;
+			for (let i = 0; i < siblings.length; i++) {
+				let first = siblings[i].firstChild;
+				if(first !== el && first.tagName == 'INPUT') {
+					first.checked = false;
+				}
+			}
+		}
+	}
+
+	let input_check = Array.prototype.slice.call(document.querySelectorAll('#data_calc input:checked'));
+	let result = 0;
+	input_check.forEach( (input) => {
+		result += parseInt(input.value);
+		console.log(input);	
+	})
+	console.log(price);	
+	price.textContent = result;
+}
+
+var inputs_calc = Array.prototype.slice.call(document.querySelectorAll('#calculator input'));
+var view = Array.prototype.slice.call(document.querySelectorAll('#view input'));
+inputs_calc.forEach( (input) => {
+	let name = input.getAttribute('name');
+	
+	let changeGlaz = () => {
+		let glaz = document.querySelector('#glazing input:checked');
+		let check;
+		view.forEach( (el) => {
+			if (el.checked) check = el;
+			el.checked = false;
+			if (glaz.value == el.getAttribute('name')) {
+				el.setAttribute('type', 'radio');
+			}else{
+				el.setAttribute('type', 'hidden');
+			}
+		})
+		let siblings = check.parentElement.children;
+		for (let i = 0; i < siblings.length; i++) {
+			if(siblings[i] !== check && siblings[i].tagName == 'INPUT') {
+				siblings[i].checked = true;
+			}
+		}
+	}
+
+	if(name == 'glazing') {
+		input.addEventListener("change", changeGlaz, false);
+	}
+	input.addEventListener("change", addPrice, false);
+})
+
+addPrice();
+
+var inputs = Array.prototype.slice.call(document.querySelectorAll('.phone-mask'));
+inputs.forEach( function(input) {
+	var keyCode;
+	function mask(event) {
+		event.keyCode && (keyCode = event.keyCode);
+		var pos = this.selectionStart;
+		if (pos < 4) event.preventDefault();
+		var matrix = "+7 (___) ___-____",
+				i = 0,
+				def = matrix.replace(/\D/g, ""),
+				val = this.value.replace(/\D/g, ""),
+				new_value = matrix.replace(/[_\d]/g, function(a) {
+					return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+											});
+				i = new_value.indexOf("_");
+		if (i != -1) {
+			i < 5 && (i = 4);
+			new_value = new_value.slice(0, i)
+		}
+		var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+			function(a) {
+				return "\\d{1," + a.length + "}"
+			}).replace(/[+()]/g, "\\$&");
+		reg = new RegExp("^" + reg + "$");
+		if (!reg.test(this.value) || this.value.length < 6 || keyCode > 47 && keyCode < 58) this.value = new_value;
+		if (event.type == "blur" && this.value.length < 6)  this.value = "";
+	}
+
+	input.addEventListener("input", mask, false);
+	input.addEventListener("focus", mask, false);
+	input.addEventListener("blur", mask, false);
+	input.addEventListener("keydown", mask, false)
+})
 
 
 function initMap(coords) {
